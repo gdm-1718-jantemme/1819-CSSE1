a506db4fc1a5017a5809bccfe4d5fbce99db413f
Jan Temmerman <jantemme@student.arteveldehs.be>
2018-10-22 17:54:41 +0200

    Lesweek 4, Faculteit en Fibonacci

---
+++ lesweek4/Faculteit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Faculteit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = 1;
+            for(int i = 0; i <= 20; i++)
+            {
+                a = a * i;
+                if(a == 0)
+                    a = 1;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
