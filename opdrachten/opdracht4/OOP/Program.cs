713ade0096bae23fb84201792035953263d97bbb
Jan Temmerman <jantemme@student.arteveldehs.be>
2018-11-17 16:19:08 +0100

    WIP

---
+++ opdrachten/opdracht4/OOP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public class MenuItems
+        {
+            
+        }
+
+        public class Product : MenuItems
+        {
+            string naam, type;
+            string[] ingediënten;
+            public Product(string naam, string type, string[] ingediënten)
+            {
+                this.naam = naam;
+                this.type = type;
+                this.ingediënten = ingediënten;
+            }
+        }
+        public class Prijs : MenuItems
+        {
+            string valuta;
+            decimal bedrag;
+            public Prijs(string valuta, decimal bedrag)
+            {
+                this.valuta = valuta;
+                this.bedrag = bedrag;
+            }
+        }
+    }
+}
